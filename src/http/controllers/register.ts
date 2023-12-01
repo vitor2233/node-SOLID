d31f2a5407534247ad3c82d2455b7a61e02d393a
vitor2233 <vitorguilherme2233@gmail.com>
2023-11-30 22:53:42 -0300

    Design patterns

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+        await registerUseCase.execute({ name, email, password })
+    } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: err.message })
+        }
+
+        return reply.status(500).send()
+    }
+
+    return reply.status(201).send()
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { UserAlreadyExistsError } from '@/errors/user-already-exists-error';
+import { UsersRepository } from '@/repositories/user-repository';
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+    name: string;
+    email: string;
+    password: string
+}
+
+export class RegisterUseCase {
+    constructor(private usersRepository: UsersRepository) { }
+
+    async execute({
+        name, email, password
+    }: RegisterUseCaseRequest) {
+        const password_hash = await hash(password, 6)
+
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+        if (userWithSameEmail) {
+            throw new UserAlreadyExistsError()
+        }
+
+        await this.usersRepository.create({
+            name,
+            email,
+            password_hash
+        })
+
+    }
+}
